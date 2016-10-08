f5d617029b8d9203b799e938bfab1a6ee3dc0c8a
luffylg <lake0105@foxmail.com>
2016-10-08 18:09:43 +0800

    two_sum

---
+++ src/Reverse_String/Solution.java
@@ -0,0 +1,26 @@
+package Reverse_String;
+
+/**
+ * Created by hp on 2016/10/8.
+ */
+
+public class Solution {
+    public String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length/2; i++) {
+            char a=chars[chars.length-i-1];
+            chars[chars.length-i-1]=chars[i];
+            chars[i]=a;
+        }
+//        String reverse="";
+//        for (int i=0;i<s.length();i++) {
+//            reverse=s.charAt(i)+reverse;
+//        }
+        return String.valueOf(chars);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).reverseString("acs  adffsdd"));
+    }
+}
