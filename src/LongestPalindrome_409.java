9ada277cca5509822bee56699816525815a313aa
luffylg <lake0105@foxmail.com>
2016-10-10 15:12:01 +0800

    wrong answer

---
+++ src/LongestPalindrome_409.java
@@ -0,0 +1,41 @@
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by hp on 2016/10/10.
+ */
+public class LongestPalindrome_409 {
+    public int longestPalindrome(String s) {
+        int sum=0;
+        int ji=0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for (int i = 0; i <s.length() ; i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }else {
+                map.put(c,1);
+            }
+        }
+        for (Object o : map.entrySet()) {
+            Map.Entry entry = (Map.Entry) o;
+            Object val = entry.getValue();
+            int a=(int)val;
+            if (a%2==1){
+                if (a>ji){
+                    sum-=ji;
+                    ji=a;
+                    sum += ji;
+                }
+            }else sum+=a;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String st = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        System.out.println(new LongestPalindrome_409().longestPalindrome(st));
+    }
+}
