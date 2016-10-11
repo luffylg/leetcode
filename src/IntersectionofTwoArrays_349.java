63646e3fa8a31384f3ae01be8bd659b758f98387
luffylg <lake0105@foxmail.com>
2016-10-11 16:36:36 +0800

    intersection of two arrays with Hashset

--- src/IntersectionofTwoArrays_349.java
+++ src/IntersectionofTwoArrays_349.java
@@ -8,24 +8,19 @@ import java.util.List;
  */
 public class IntersectionofTwoArrays_349 {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i:nums1) set.add(i);
         ArrayList<Integer> list3 = new ArrayList<Integer>();
-        for (int i:nums1) list1.add(i);
-        for (int i:nums2) list2.add(i);
-        while (list1.size()!=0){
-            int val=list1.get(0);
-            list1.remove((Integer) val);
-            if (list2.contains(val)&&!list3.contains(val)){
-                list3.add(val);
+        for (int j:nums2) {
+            if (set.contains(j)){
+                set.remove(j);
+                list3.add(j);
             }
         }
-        return toIntArray(list3);
-    }
-    private int[] toIntArray(List<Integer> list){
-        int[] ret = new int[list.size()];
-        for(int i = 0;i < ret.length;i++)
-            ret[i] = list.get(i);
+        int[] ret = new int[list3.size()];
+        for (int i = 0; i <list3.size() ; i++) {
+            ret[i]=list3.get(i);
+        }
         return ret;
     }
 }
