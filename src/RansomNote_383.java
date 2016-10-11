2f20786737cc5835bce277f8096e74ce670f1577
luffylg <lake0105@foxmail.com>
2016-10-11 13:51:23 +0800

    faster with array and foreach

--- src/RansomNote_383.java
+++ src/RansomNote_383.java
@@ -8,11 +8,11 @@ import java.util.List;
 public class RansomNote_383 {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] table=new int[26];
-        for (int i = 0; i <magazine.length() ; i++) {
-            table[magazine.charAt(i)-'a']++;
+        for (char s:magazine.toCharArray()) {
+            table[s-'a']++;
         }
-        for (int i = 0; i <ransomNote.length() ; i++) {
-            if (--table[ransomNote.charAt(i)-'a']<0) return false;
+        for (char s:ransomNote.toCharArray()) {
+            if(--table[s-'a']<0) return false;
         }
         return true;
     }
