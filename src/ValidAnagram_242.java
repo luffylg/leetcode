6181b2c3d36c11ca337526c0dac93bf092cf4516
luffylg <lake0105@foxmail.com>
2016-10-12 16:27:08 +0800

    slow answer

--- src/ValidAnagram_242.java
+++ src/ValidAnagram_242.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by hp on 2016/10/12.
  */
 public class ValidAnagram_242 {
     public boolean isAnagram(String s, String t) {
-        int sum = 0;
-        for (Character i : s.toCharArray()) sum ^= (int)i;
-        for (Character i : t.toCharArray()) sum ^= (int)i;
-        return sum == 0;
+        if (s.length()!=t.length()) return false;
+        char[] chars = s.toCharArray();
+        ArrayList<Character> list = new ArrayList<>();
+        for (Character i:chars) {
+            list.add(i);
+        }
+        for (int j=0;j<t.length();j++) {
+            char c = t.charAt(j);
+            if (list.contains(c)) list.remove((Character) c);
+            else return false;
+        }
+        return true;
     }
 }
