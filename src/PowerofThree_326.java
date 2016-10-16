52f69c7a696e1e57807e14882e76f86439216a0f
luffylg <luffylg05@gmail.com>
2016-10-16 22:33:54 +0800

    power of three with loop

--- src/PowerofThree_326.java
+++ src/PowerofThree_326.java
@@ -1,9 +1,11 @@
 
 public class PowerofThree_326 {
 	public boolean isPowerOfThree(int n) {
-		if (n<=0) return false;
-		if (n==1) return true;
-		if (n%3!=0) return false;
-        return isPowerOfThree(n/3);
+		while (true) {
+			if(n<=0) return false;
+			if (n==1) return true;
+			if (n%3!=0) return false;
+			n=n/3;
+		}
     }
 }
