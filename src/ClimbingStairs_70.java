1c9572e77e4e21d850ebd1eee484d0a0ca538b61
luffylg <lake0105@foxmail.com>
2016-10-18 16:24:24 +0800

    method with array

--- src/ClimbingStairs_70.java
+++ src/ClimbingStairs_70.java
@@ -6,7 +6,13 @@ public class ClimbingStairs_70 {
         if (n<=0) return 0;
         if (n==1) return 1;
         if (n==2) return 2;
-        return climbStairs(n-1)+climbStairs(n-2);
+        int[] ints = new int[n];
+        ints[0]=1;
+        ints[1]=2;
+        for (int i = 2; i <n ; i++) {
+            ints[i]=ints[i-1]+ints[i-2];
+        }
+        return ints[n-1];
     }
 
     public static void main(String[] args) {
