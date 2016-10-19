a2fbdd901a5c33186f26c992e50645a0ee933925
luffylg <lake0105@foxmail.com>
2016-10-19 15:56:32 +0800

    merge two sorted lists

---
+++ src/MergeTwoSortedLists_21.java
@@ -0,0 +1,21 @@
+/**
+ * Created by hp on 2016/10/19.
+ */
+public class MergeTwoSortedLists_21 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1==null) return l2;
+        if (l2==null) return l1;
+        if (l1.val<l2.val){
+            l1.next=mergeTwoLists(l1.next,l2);
+            return l1;
+        }else {
+            l2.next=mergeTwoLists(l2.next,l1);
+            return l2;
+        }
+    }
+}
