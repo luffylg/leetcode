2e2e6e43aa8e7c6ae8aa64856a02151b88601d27
luffylg <lake0105@foxmail.com>
2016-10-21 17:34:13 +0800

    binary tree level order tracersal dfs

--- src/BinaryTreeLevelOrderTraversal_102.java
+++ src/BinaryTreeLevelOrderTraversal_102.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by hp on 2016/10/21.
@@ -13,15 +15,19 @@ public class BinaryTreeLevelOrderTraversal_102 {
     }
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> lists = new ArrayList<List<Integer>>();
-        int level=0;
-        order(root,lists,level);
+        if (root==null) return lists;
+        Queue<TreeNode> qe=new LinkedList<>();
+        qe.offer(root);
+        while (!qe.isEmpty()){
+            int len=qe.size();
+            ArrayList<Integer> integers = new ArrayList<>();
+            for (int i = 0; i <len ; i++) {
+                if (qe.peek().left!=null) qe.offer(qe.peek().left);
+                if (qe.peek().right!=null) qe.offer(qe.peek().right);
+                integers.add(qe.poll().val);
+            }
+            lists.add(integers);
+        }
         return lists;
-    }
-    private void order(TreeNode root, List<List<Integer>> lists, int level) {
-        if (root==null) return;
-        if (level>=lists.size()) lists.add(new ArrayList<Integer>());
-        lists.get(level).add(root.val);
-        order(root.left,lists,level+1);
-        order(root.right,lists,level+1);
     }
 }
