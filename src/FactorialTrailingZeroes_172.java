cd97f12ac74f783146477bcff2bb1aa56d8e01d0
luffylg <luffylg05@gmail.com>
2016-10-27 22:39:59 +0800

    factorial trailing zeros

--- src/FactorialTrailingZeroes_172.java
+++ src/FactorialTrailingZeroes_172.java
@@ -1,4 +1,15 @@
 
 public class FactorialTrailingZeroes_172 {
+	public int trailingZeroes(int n) {
+        if (n<5) {
+			return 0;
+		}
+        int sum=0;
+        while (n>=5) {
+			n/=5;
+			sum+=n;
+		}
+        return sum;
+    }
 
 }
