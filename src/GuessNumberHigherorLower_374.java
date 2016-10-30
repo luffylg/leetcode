8fcccca38393119a79c128153020b2b2c4d38000
luffylg <luffylg05@gmail.com>
2016-10-30 19:26:03 +0800

    guess number higher or lower

--- src/GuessNumberHigherorLower_374.java
+++ src/GuessNumberHigherorLower_374.java
@@ -1,4 +1,17 @@
 
-public class GuessNumberHigherorLower_374 {
-
+public class GuessNumberHigherorLower_374 extends GuessGame {
+	public int guessNumber(int n) {
+		return gs(1,n);
+    }
+	private int gs(int start,int end){
+		int num=(start+end)/2;
+		int flag=guess(num);
+		if (flag==0) {
+			return num;
+		}
+		if (flag==1) {
+			return gs(start,num);
+		}
+		return gs(num+1,end);
+	}
 }
