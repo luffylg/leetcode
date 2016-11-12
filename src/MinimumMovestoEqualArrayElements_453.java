8bbd75c43d78265d01bde3ab23b46a1d26ebc5cc
luffylg <luffylg05@gmail.com>
2016-11-12 09:46:41 +0800

    minimum moves to equal array elements

--- src/MinimumMovestoEqualArrayElements_453.java
+++ src/MinimumMovestoEqualArrayElements_453.java
@@ -1,9 +1,13 @@
 
 public class MinimumMovestoEqualArrayElements_453 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public int minMoves(int[] nums) {
+		int min=Integer.MAX_VALUE;
+		int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+			min=Math.min(min, nums[i]);
+			sum+=nums[i];
+		}
+        return sum-nums.length*min;
+    }
 
 }
