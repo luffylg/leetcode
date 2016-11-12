ff275a13dda321b10f9f4ab41b9608222a54c36c
luffylg <luffylg05@gmail.com>
2016-11-12 23:32:07 +0800

    valid parentheses

--- src/ValidParentheses_20.java
+++ src/ValidParentheses_20.java
@@ -1,21 +1,43 @@
+import java.util.Stack;
 
 public class ValidParentheses_20 {
 	public boolean isValid(String s) {
         if (s.length()%2!=0) {
 			return false;
 		}
-        for (int i = 0; i < s.length(); i+=2) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
 			char ch=s.charAt(i);
-			if (ch=='[') {
-				if(s.charAt(i+1)!=']') return false;
-			}else if (ch=='{') {
-				if(s.charAt(i+1)!='}') return false;
-			}else if (ch=='(') {
-				if(s.charAt(i+1)!=')') return false;
-			}else {
-				return false;
+			switch (ch) {
+			case ']':
+				if (stack.isEmpty()) {
+					return false;
+				}
+				if (stack.pop()!='[') {
+					return false;
+				}
+				break;
+			case '}':
+				if (stack.isEmpty()) {
+					return false;
+				}
+				if (stack.pop()!='{') {
+					return false;
+				}
+				break;
+			case ')':
+				if (stack.isEmpty()) {
+					return false;
+				}
+				if (stack.pop()!='(') {
+					return false;
+				}
+				break;
+
+			default:
+				stack.push(ch);
 			}
 		}
-        return true;
+        return stack.isEmpty();
     }
 }
