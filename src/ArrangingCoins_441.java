6a6bc77e89cc8124ebdcadcc39e9d47cf3508b72
luffylg <luffylg05@gmail.com>
2016-11-12 09:55:23 +0800

    arranging coins

--- src/ArrangingCoins_441.java
+++ src/ArrangingCoins_441.java
@@ -1,14 +1,17 @@
 
 public class ArrangingCoins_441 {
 	public int arrangeCoins(int n) {
-		long sum=0;
+		if (n==0) {
+			return 0;
+		}
+		int sum=0;
 		int i=1;
 		for (; ; i++) {
-			sum=i*(i+1)/2;
-			if (n<sum) {
+			sum+=i;
+			if (n-sum<i+1) {
 				break;
 			}
 		}
-		return i-1;
+		return i;
     }
 }
