39593ab582e7a4a94bdb12b1f4eaf51f50313eeb
luffylg <lake0105@foxmail.com>
2016-12-01 21:09:46 +0800

    method with hashset

--- src/IslandPerimeter_463.java
+++ src/IslandPerimeter_463.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Created by hp on 2016/12/1.
  */
 public class IslandPerimeter_463 {
     public int islandPerimeter(int[][] grid) {
-        if (grid.length==0) return 0;
         int sum=0;
-        int[] lastrow = new int[grid[0].length];
+        HashSet<Integer> lastrow = new HashSet<>();
         for (int[] aGrid : grid) {
-            int[] thisrow = new int[aGrid.length];
+            HashSet<Integer> thisrow = new HashSet<>();
             for (int j = 0; j < aGrid.length; j++) {
                 int cell = aGrid[j];
                 if (cell == 1) {
                     sum += 4;
-                    if (j >= 1 && thisrow[j - 1] == 1) sum -= 2;
-                    if (lastrow[j] == 1) sum -= 2;
-                    thisrow[j] = 1;
+                    if (thisrow.contains(j-1)) sum -= 2;
+                    thisrow.add(j);
+                    if (lastrow.contains(j)) sum -= 2;
                 }
             }
             lastrow = thisrow;
