adb94838979214b2926a839ab6e608499a369aa2
luffylg <lake0105@foxmail.com>
2016-12-01 20:21:30 +0800

    circle method

--- src/PascalsTriangleII_119.java
+++ src/PascalsTriangleII_119.java
@@ -7,13 +7,12 @@ import java.util.List;
 public class PascalsTriangleII_119 {
     public List<Integer> getRow(int rowIndex) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        if (rowIndex==0) return list;
-        List<Integer> row = getRow(rowIndex - 1);
-        for (int i = 1; i <rowIndex ; i++) {
-            list.add(row.get(i-1)+row.get(i));
+        for (int i = 0; i <=rowIndex ; i++) {
+            list.add(0,1);
+            for (int j = 1; j <list.size()-1 ; j++) {
+                list.set(j, list.get(j) + list.get(j + 1));
+            }
         }
-        list.add(1);
         return list;
     }
 }
