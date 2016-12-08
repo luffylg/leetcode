1075c4091ba68c8c1618f75ade4d962611f4bddb
luffylg <lake0105@foxmail.com>
2016-12-08 22:47:03 +0800

    intersection of two linkedlists

---
+++ src/IntersectionofTwoLinkedLists_160.java
@@ -0,0 +1,25 @@
+/**
+ * Created by hp on 2016/12/8.
+ */
+
+
+public class IntersectionofTwoLinkedLists_160 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA==null||headB==null) return null;
+        ListNode nodea=headA;
+        ListNode nodeb=headB;
+        while (nodea!=nodeb){
+            nodea=nodea==null?headB:nodea.next;
+            nodeb=nodeb==null?headA:nodea.next;
+        }
+        return nodea;
+    }
+}
