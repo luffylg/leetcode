e064c9e3fbe158372a8621d203a28ff55b1a82e5
luffylg <lake0105@foxmail.com>
2016-12-08 20:46:30 +0800

    number of boomerangs

--- src/NumberofBoomerangs_447.java
+++ src/NumberofBoomerangs_447.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Created by hp on 2016/12/8.
  */
@@ -5,17 +7,20 @@ public class NumberofBoomerangs_447 {
     public int numberOfBoomerangs(int[][] points) {
         if (points.length<3) return 0;
         int count=0;
-        for (int i = 0; i <points.length; i++) {
-            for (int j = 0; j <points.length; j++) {
+        for (int i = 0; i <points.length;  i++) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = 0; j <points.length ; j++) {
                 if (j==i) continue;
-                for (int k = j+1; k <points.length ; k++) {
-                    if (k==i) continue;
-                    if (Math.pow(points[i][0]-points[j][0],2)+Math.pow(points[i][1]-points[j][1],2)==Math.pow(points[i][0]-points[k][0],2)+Math.pow(points[i][1]-points[k][1],2)){
-                        count+=2;
-                    }
+                int length= (int) (Math.pow(points[i][0]-points[j][0],2)+Math.pow(points[i][1]-points[j][1],2));
+                if (!map.containsKey(length)) map.put(length,1);
+                else {
+                    count=count+2*map.get(length);
+                    map.put(length,map.get(length)+1);
                 }
             }
+
         }
+
         return count;
     }
 
