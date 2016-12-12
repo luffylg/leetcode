ac55c53fd73b757f2327c9575e9a36bd2c8a79f4
luffylg <lake0105@foxmail.com>
2016-12-12 15:27:03 +0800

    bit manipulation

--- src/Numberof1Bits_191.java
+++ src/Numberof1Bits_191.java
@@ -5,14 +5,14 @@ public class Numberof1Bits_191 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int sum=0;
-        while (n>0){
-            sum+=n%2;
-            n/=2;
+        while (n!=0){
+            sum+=n&1;
+            n = n >>> 1;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Numberof1Bits_191().hammingWeight(2147483647));
+        System.out.println(new Numberof1Bits_191().hammingWeight(-2147483648));
     }
 }
