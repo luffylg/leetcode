7cae9d63165df227f5ddcd4ac19a7276c034a894
luffylg <lake0105@foxmail.com>
2016-12-12 15:24:47 +0800

    wrong answer not considering unsigned value

---
+++ src/Numberof1Bits_191.java
@@ -0,0 +1,18 @@
+/**
+ * Created by hp on 2016/12/12.
+ */
+public class Numberof1Bits_191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum=0;
+        while (n>0){
+            sum+=n%2;
+            n/=2;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Numberof1Bits_191().hammingWeight(2147483647));
+    }
+}
