18b6710b7857794758d24d7acd46bb1bcbdc949b
luffylg <lake0105@foxmail.com>
2016-12-13 23:33:07 +0800

    also time limit

--- src/Heaters_475.java
+++ src/Heaters_475.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by hp on 2016/12/13.
  */
@@ -5,10 +7,12 @@ public class Heaters_475 {
     // 1. find the closest heater of each house and store the distance
     // 2. find the longest distance
     public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
         int max=0;
         for (int house:houses) {
             int distance=Integer.MAX_VALUE;
             for (int heater:heaters) {
+                if (heater-house>distance) break;
                 distance=Math.min(Math.abs(heater-house),distance);
             }
             max=Math.max(distance,max);
