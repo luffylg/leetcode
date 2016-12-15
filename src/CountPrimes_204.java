08d69c3ecefe967612acce6b0848a8fc862e996a
luffylg <lake0105@foxmail.com>
2016-12-15 16:57:09 +0800

    Sieve of Eratosthenes

--- src/CountPrimes_204.java
+++ src/CountPrimes_204.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
  */
 public class CountPrimes_204 {
     public int countPrimes(int n) {
-        if (n<=2) return 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(2);
-        for (int num = 3; num <n ; num+=2) {
-            if (isPrime(num, list)) list.add(num);
+        boolean[] notPrime = new boolean[n];
+        int count=0;
+        for (int i = 2; i * i < n; i++) {
+            if (notPrime[i]) continue;
+            for (int j = i; j * i < n; j++) {
+                notPrime[i * j] = true;
+            }
         }
-        return list.size();
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) count++;
+        }
+        return count;
     }
 
-    private boolean isPrime(int num, ArrayList<Integer> list) {
-        double sqrt = Math.sqrt(num);
-        for (int cmp:list) {
-            if (cmp>sqrt) break;
-            if (num%cmp==0) return false;
-        }
-        return true;
+    public static void main(String[] args) {
+        System.out.println(new CountPrimes_204().countPrimes(499979));
     }
 }
