b047d994548e19d33b243b398360e7ff40d0b389
luffylg <root@luffylg.cn>
2017-01-11 17:26:41 +0800

    %02d and Integer.bitCount

--- src/BinaryWatch_401.java
+++ src/BinaryWatch_401.java
@@ -9,23 +9,11 @@ public class BinaryWatch_401 {
         LinkedList<String> result = new LinkedList<>();
         for (int i = 0; i <12 ; i++) {
             for (int j = 0; j <60 ; j++) {
-                if (bitcount(i)+bitcount(j)==num){
-                    String add=String.valueOf(i)+":";
-                    if (j<10) add=add+"0";
-                    add+=String.valueOf(j);
-                    result.add(add);
+                if (Integer.bitCount(i)+Integer.bitCount(j)==num){
+                    result.add(String.format("%d:%02d",i,j));
                 }
             }
         }
         return result;
-    }
-
-    private int bitcount(int i) {
-        String binaryString = Integer.toBinaryString(i);
-        int sum=0;
-        for (char ch:binaryString.toCharArray()) {
-            if (ch=='1') sum+=1;
-        }
-        return sum;
     }
 }
